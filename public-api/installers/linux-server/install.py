b016371d90b1521fca63194bc9f636b1b5ea3c31
aludayalu <pl46u3.ducklinz@gmail.com>
2024-07-16 03:12:02 +0530

    linux-server install script updated

--- public-api/installers/linux-server/install.py
+++ public-api/installers/linux-server/install.py
@@ -1,8 +1,16 @@
-import os
+import os, base64
 
-# Disable Wayland
+try:
+    os.mkdir("/rolldog")
+    open("/rolldog/main.py", "a").write(base64.b64decode("{server_main.py}".encode()).decode())
+except:
+    pass
 
 os.system("cp /etc/gdm3/custom.conf /etc/gdm3/custom.conf1")
+os.system("sudo apt install python3-pip -y")
+
+os.system("pip3 install flask")
+os.system("pip3 install flask --break-system-packages")
 
 open("/etc/gdm3/custom.conf", "wb").write("""
 [daemon]
@@ -19,3 +27,23 @@ WaylandEnable=false
 
 os.system("export DISPLAY=:1")
 
+open("/etc/systemd/system/rolldog.service", "wb").write(b"""
+[Unit]
+Description=Rolldog
+After=network.target
+
+[Service]
+User=root
+Type=simple
+WorkingDirectory=/rolldog
+Environment="DISPLAY=:1"
+ExecStart=sudo -u aludayalu python3 /rolldog/main.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"""[1:-1])
+os.system("sudo systemctl daemon-reload")
+os.system("sudo systemctl enable rolldog.service")
+os.system("sudo systemctl start rolldog service")
+os.system("sudo systemctl status rolldog.service")
--- public-api/main.py
+++ public-api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_file
 from werkzeug.utils import safe_join
+import base64
 
 app=Flask(__name__)
 
@@ -8,6 +9,8 @@ def sanitize(s):
 
 @app.route('/install/<path:path>')
 def installer(path):
-    return send_file("installers/"+sanitize(path)+"/install.py")
+    if len(sanitize(path))!=len(path):
+        return "path is not safe"
+    return open("installers/"+sanitize(path)+"/install.py").read().replace("{server_main.py}", base64.b64encode(open("../"+sanitize(path)+"/main.py").read().encode()).decode())
 
 app.run(host="0.0.0.0", port=8080)
