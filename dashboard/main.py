2133246d55dfee2b14d8a04008ce5b5209b21d8e
aludayalu <pl46u3.ducklinz@gmail.com>
2024-07-21 21:23:38 +0530

    dashboard renamed

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,11 +5,11 @@ import os
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.get("/computer")
 def home():
     signals=open("public/signals.js").read()
     ip="http://127.0.0.1:1234"
     indexjs=render("public/index.js", locals())
-    return render("index", locals())
+    return render("computer", locals())
 
 app.run(host="0.0.0.0", port=int(sys.argv[1]))
