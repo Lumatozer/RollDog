4cc04b67782964cd9e1711489e9d98bef65d0691
aludayalu <pl46u3.ducklinz@gmail.com>
2024-07-22 04:50:51 +0530

    fixing condition

--- linux-server/main.py
+++ linux-server/main.py
@@ -10,7 +10,7 @@ def start_services():
     while True:
         start=time.time()
         os.system("python3 /rolldog/screenshotter.py")
-        if time.time()-start<30:
+        if time.time()-start>30:
             continue
         break
 
